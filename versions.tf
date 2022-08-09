ac25dd95201c7f7cf594eba5377469796aa2547a
Kastriot Dobratiqi <kastriotdobratiqi@live.com>
2022-08-09 14:08:16 +0200

    left only the requirements

--- versions.tf
+++ versions.tf
@@ -1,16 +1,10 @@
-# terraform {
-#   required_version = ">= 0.14.10"
+terraform {
+  required_version = ">= 0.14.10"
 
-#   backend "s3" {}
-
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 3.0"
-#     }
-#   }
-# }
-
-# provider "aws" {
-#   region = var.region
-# }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
