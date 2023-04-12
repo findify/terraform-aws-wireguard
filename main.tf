c5a41fe05d356558b5f7a841cf1ac261cb818872
Sven Lange <sven@findify.io>
2023-04-12 08:34:58 +0200

    Use templatefile for templating
    The data resource `template_file` has been deprecated a long time ago.

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+locals {
+  wg_client_data_json = [
+    for client in var.wg_clients : templatefile(
+      "${path.module}/templates/client-data.tpl",
+      {
+        friendly_name = client.friendly_name,
+        client_pub_key = client.public_key,
+        client_ip = client.client_ip,
+        persistent_keepalive = var.wg_persistent_keepalive,
+      }
+    )
+  ]
+}
+
 resource "aws_eip" "wireguard" {
   vpc = true
   tags = {
@@ -12,18 +26,6 @@ resource "aws_route53_record" "wireguard" {
   type            = "A"
   ttl             = "60"
   records         = [aws_eip.wireguard.public_ip]
-}
-
-data "template_file" "wg_client_data_json" {
-  template = file("${path.module}/templates/client-data.tpl")
-  count    = length(var.wg_clients)
-
-  vars = {
-    friendly_name        = var.wg_clients[count.index].friendly_name
-    client_pub_key       = var.wg_clients[count.index].public_key
-    client_ip            = var.wg_clients[count.index].client_ip
-    persistent_keepalive = var.wg_persistent_keepalive
-  }
 }
 
 data "aws_ami" "ubuntu" {
@@ -50,7 +52,7 @@ resource "aws_launch_configuration" "wireguard_launch_config" {
     wg_server_private_key_aws_ssm_name = var.use_ssm ? aws_ssm_parameter.wireguard_server_private_key[0].name : null,
     wg_server_net                      = var.wg_server_net,
     wg_server_port                     = var.wg_server_port,
-    peers                              = join("\n", data.template_file.wg_client_data_json.*.rendered),
+    peers                              = join("\n", local.wg_client_data_json),
     use_eip                            = var.use_eip ? "enabled" : "disabled",
     eip_id                             = aws_eip.wireguard.id,
     use_ssm                            = var.use_ssm ? "true" : "false",
